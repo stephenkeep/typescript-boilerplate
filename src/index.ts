a3bb7b0440e232a1409917cca849bd688b836e1d
Stephen Keep <stephenkeep@gmail.com>
2021-05-13 12:48:39 +0930

    enable debugging in chrome

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+//import { User } from './import';
+
+const user = {
+  id: 'test',
+};
+
+console.log(user.id);
+
 const add = (num1: number, num2: number) => num1 + num2;
 console.log(add(1, 1));
